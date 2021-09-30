c169fc93c9083552713afe16bfcdfa54bf080b41
JinYongKim <charlie0819@naver.com>
2021-09-30 12:25:09 +0000

    baseline code

---
+++ mmdetection/configs/_kjy_/cascade_rcnn_r50_fpn_1x_coco.py
@@ -0,0 +1,6 @@
+_base_ = [
+    '../_custom_/models/cascade_rcnn_r50_fpn.py',
+    '../_custom_/datasets/coco_detection.py',
+    '../_custom_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+]
+
